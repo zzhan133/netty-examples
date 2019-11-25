b37b95b29a70e24215a343f0f5d246ef50b83b49
Zhe Zhang <zzhan133@asu.edu>
2019-11-24 17:02:02 -0700

    Add checkstyle configuration file

---
+++ src/main/java/com/gila/springs/server/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package com.gila.springs.server;
+
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * EchoServerHandler
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // TODO Auto-generated method stub
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // TODO Auto-generated method stub
+        super.exceptionCaught(ctx, cause);
+    }
+}
